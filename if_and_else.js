b86339d459c61f8ca3bd31f350861d02e48cf3ce
Fahmidur-arnob <arnobdev93@gmail.com>
2022-07-31 21:55:12 +0600

    Added some lines of code

---
+++ if_and_else.js
@@ -0,0 +1,19 @@
+let iPhonePrice = 79000;
+let myBudget = 9500;
+
+if(iPhonePrice < myBudget){
+    console.log("I will but iPhone");
+}
+else{
+    console.log("Nokia 1100");
+}
+
+let chickenPrice = 500;
+let myMoney = 499;
+
+if(chickenPrice <= myMoney){
+    console.log("Murgir ran khamu and haddi chabamu");
+}
+else{
+    console.log("alu vorta diya khamu");
+}
