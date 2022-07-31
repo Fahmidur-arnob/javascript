1e3e32790d8006b09858dfbfac2c5521f7b085d8
Fahmidur-arnob <arnobdev93@gmail.com>
2022-07-31 22:54:08 +0600

    Added some lines of code

---
+++ multi_stage_condition.js
@@ -0,0 +1,6 @@
+let money = 100;
+let danishPrice = 45;
+let butterBread = 35;
+let toastBiscuit = 20;
+
+
---
+++ multiple_condition.js
@@ -0,0 +1,21 @@
+// let isGraduated = true;
+
+// let salary = 95000;
+// let cars = 1;
+// if(isGraduated == true && salary > 50000 && cars >=1 ){
+//     console.log("Congratulation");
+// }
+// else{
+//     console.log("Keep up the grind G!");
+// }
+
+let isGraduated = false;
+let salary = 31000;
+let cars = 3;
+
+if(isGraduated === true || salary > 50000 || cars >= 1){
+    console.log("Congratulation");
+}
+else{
+    console.log("Work Harder My G!");
+}
