ca18ba957ac9b17b527b523a22319e555fa60941
Fahmidur-arnob <arnobdev93@gmail.com>
2022-08-02 13:02:19 +0600

    Tried some reversing

---
+++ each_element_for_loop.js
@@ -0,0 +1,8 @@
+
+
+let numbersInArray = [45, 87, 56, 32, 51, 25];
+
+for(let i = 0; i < numbersInArray.length; i++){
+    let number = numbersInArray[i];
+    console.log("Elements are => ", number);
+}
--- for_loop.js
+++ for_loop.js
@@ -1,4 +1,8 @@
 
-for(let roastGiven = 1; roastGiven <= 20; roastGiven++){
-    console.log(`Roast Den Please!`, roastGiven);
+// for(let roastGiven = 1; roastGiven <= 20; roastGiven++){
+//     console.log(`Roast Den Please!`, roastGiven);
+// }
+
+for(let i = 1; i <= 10; i++){
+    console.log(`Roast Den => `, i);
 }
