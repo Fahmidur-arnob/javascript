28834194da3872e05974af6a3ccd81f02a5aa83f
Fahmidur-arnob <arnobdev93@gmail.com>
2022-08-06 21:14:06 +0600

    Done with String

--- strings-and-things/search_in_js.js
+++ strings-and-things/search_in_js.js
@@ -12,37 +12,18 @@ boshonto kale tomay bolte parini. shad";
 //     console.log("NO");
 // }
 
-// function checkingIfSomeWordExistInStringOrNot(s) {
+// function doesExist(s) {
 //     let n = s.length;
-
 //     for (let i = 1; i < n; i++) {
-//         if (s.includes("shada")) {
+//         if (s.match("shada")) {
 //             return true;
 //         }
 //     }
 //     return false;
 // }
-
-// if (checkingIfSomeWordExistInStringOrNot(lyrics)) {
+// if (doesExist(lyrics)) {
 //     console.log("YES");
 // }
 // else {
-//     console.log("NO")
+//     console.log("NO");
 // }
-
-function doesExist(s) {
-    let checker = "shada";
-    let n = s.length;
-    for (let i = 1; i < n; i++) {
-        if (s.match("shada")) {
-            return true;
-        }
-    }
-    return false;
-}
-if (doesExist(lyrics)) {
-    console.log("YES");
-}
-else {
-    console.log("NO");
-}
