0097437d84838dbe9a76d6b07234578c020c89b0
Fahmidur-arnob <arnobdev93@gmail.com>
2022-08-06 20:32:24 +0600

    Made some files also learned strings

---
+++ strings-and-things/case_conversion_in_js.js
@@ -0,0 +1,11 @@
+
+const userName = 'BlackPink';
+
+const userInput = 'BlackPinK';
+
+if(userName == userInput){
+    console.log("YES");
+}
+else{
+    console.log("NO");
+}
--- while_loop.js
+++ while_loop.js
@@ -1,8 +1,11 @@
 
-let numOne = 67;
-let numTwo = 89;
-let i = 0;
-while(i != numOne){
-    console.log("the value of i is: ", i);
-    i++;
-}
+// let numOne = 67;
+// let numTwo = 89;
+// let i = 0;
+// while(i != numOne){
+//     console.log("the value of i is: ", i);
+//     i++;
+// }
+
+let a = 50;
+console.log(a);
